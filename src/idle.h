fca1b5f336c2a632eb612441e7a0480a3f365686
Paul Betts <paul@paulbetts.org>
2015-08-17 14:35:30 -0700

    Implement idle time under Linux

--- src/idle.h
+++ src/idle.h
@@ -2,7 +2,7 @@
 #define WIN 2
 #define LIN 3
 
-#if OS == WIN
+#if OS != MAC
 #include <stdint.h>
 #endif
 
---
+++ src/linux/idle.cc
@@ -0,0 +1,29 @@
+#ifndef BUILDING_NODE_EXTENSION
+#define BUILDING_NODE_EXTENSION
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/scrnsaver.h>
+  
+static int has_extension = -1;
+
+uint32_t SystemIdleTime(void) {
+  Display *dpy = XOpenDisplay(NULL);
+  int event_base, error_base;
+
+  if (has_extension == -1) {
+    has_extension = XScreenSaverQueryExtension(dpy, &event_base, &error_base);
+  }
+
+  if (!has_extension) {
+    return 0;
+  }
+
+  XScreenSaverInfo info;
+  XScreenSaverQueryInfo(dpy, RootWindow(dpy, DefaultScreen(dpy)), &info);
+  return (info.idle);
+}
