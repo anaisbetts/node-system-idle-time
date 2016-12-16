b8f677d298a041f5ec7e5d1241e8de2337819176
Thomas Moenicke <tm243@users.noreply.github.com>
2016-12-16 12:41:17 +0100

    removing the goto in the linux code

--- src/linux/idle.cc
+++ src/linux/idle.cc
@@ -8,7 +8,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/scrnsaver.h>
-  
+
 static int has_extension = -1;
 
 uint32_t SystemIdleTime(void) {
@@ -26,21 +26,17 @@ uint32_t SystemIdleTime(void) {
     has_extension = XScreenSaverQueryExtension(dpy, &event_base, &error_base);
   }
 
-  if (!has_extension) {
-    goto out;
+  if (has_extension) {
+    wnd = RootWindow(dpy, DefaultScreen(dpy));
+
+    if (!wnd) {
+      idle_time = 60*60*1000;
+    } else {
+      XScreenSaverQueryInfo(dpy, wnd, &info);
+      idle_time = info.idle;
+    }
   }
 
-  wnd = RootWindow(dpy, DefaultScreen(dpy));
-
-  if (!wnd) {
-    idle_time = 60*60*1000;
-    goto out;
-  }
-
-  XScreenSaverQueryInfo(dpy, wnd, &info);
-  idle_time = info.idle;
-
-out:
   XCloseDisplay(dpy);
   return idle_time;
 }
